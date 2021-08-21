c0c328011cd3e1c119028325541879e2223f7d2e
Gavin Jamieson <1gavmo@gmail.com>
2021-08-22 09:04:54 +1000

    Always exclude unincorporated zones

--- scripts/geodata.py
+++ scripts/geodata.py
@@ -21,7 +21,7 @@ def process_geodata(json_obj):
     for each in json_obj["features"]:
         if each["properties"]["nsw_lga__3"] not in areas and not each["properties"]["nsw_lga__3"].startswith("UNINCOR"):
             areas[each["properties"]["nsw_lga__3"]] = each["geometry"]["coordinates"][0][0]
-        else:
+        elif not each["properties"]["nsw_lga__3"].startswith("UNINCOR"):
             # if a duplicate LGA name exists, add a number to the end.  {LGA_NAME}_{Count of dupes}
             existing_count = len([dup for dup in areas.keys() if dup.startswith(each["properties"]["nsw_lga__3"])])
             newkey = f"""{each["properties"]["nsw_lga__3"]}_{existing_count}"""
@@ -45,10 +45,22 @@ def find_limits(point_list):
     return upper, lower
 
 
+def inbounds(point_list, bounds):
+    """Check to see if at least one point is within the viewing window"""
+    for point in point_list:
+        if bounds[0][1] <= point[0] <= bounds[1][1]:
+            if bounds[1][0] <= point[1] <= bounds[0][0]:
+                return True
+
+
 def main(json_obj):
     """Convenience"""
     a = process_geodata(json_obj)
-    print(a.keys())
+    map_window = ((-33.391159, 150.501858), (-34.131313, 151.417080))
+    for k, v in a.items():
+        if inbounds(v, map_window):
+            print(k)
+
 
 if __name__ == '__main__':
     with open(sys.argv[1]) as json_file:
